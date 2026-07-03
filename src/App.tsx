ac318a66278cc6c1987592081e133785a15221c2
Grigorii Veinin <kage.grifon@yandex.ru>
2026-07-03 18:24:52 +0500

    feat: Подключить провайдеры игры и токены темы (Phase 4-5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { AppProvider } from './state/AppContext';
+import { GameProvider } from './state/GameContext';
 import GameScreen from './components/game/GameScreen';
 import './App.css';
 
 export default function App() {
-  return <GameScreen />;
+  return (
+    <AppProvider>
+      <GameProvider>
+        <GameScreen />
+      </GameProvider>
+    </AppProvider>
+  );
 }
